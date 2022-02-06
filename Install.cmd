e7678b9929cb0dc007de0fa24db64361f8437c86
Aran <aranholbrook@outlook.com>
2022-02-06 15:05:06 +0000

    Uploaded Install and Uninstall scripts for deployment

---
+++ Install.cmd
@@ -0,0 +1,6 @@
+@ECHO off
+CLS
+MD %ALLUSERSPROFILE%\Flamingo
+MD %windir%\Logs\Flamingo
+robocopy %~dp0 %ALLUSERSPROFILE%\Flamingo *.ps1 *.xaml *.ico /z /np /LOG:%windir%\Logs\Flamingo\Install.log /tee
+schtasks /create /tn "Start Flamingo" /tr "powershell -ExecutionPolicy ByPass -File %ALLUSERSPROFILE%\Flamingo\SysTray.ps1" /sc onlogon /f
