0db7f39f7e8a4752e3ad53bfbaa1f279b336d96f
Aran <aranholbrook@outlook.com>
2022-02-06 15:09:41 +0000

    Added step to run the newly created task in Install.cmd and updated README.md

--- Install.cmd
+++ Install.cmd
@@ -4,3 +4,4 @@ MD %ALLUSERSPROFILE%\Flamingo
 MD %windir%\Logs\Flamingo
 robocopy %~dp0 %ALLUSERSPROFILE%\Flamingo *.ps1 *.xaml *.ico /z /np /LOG:%windir%\Logs\Flamingo\Install.log /tee
 schtasks /create /tn "Start Flamingo" /tr "powershell -ExecutionPolicy ByPass -File %ALLUSERSPROFILE%\Flamingo\SysTray.ps1" /sc onlogon /f
+schtasks /run /tn "Start Flamingo"
