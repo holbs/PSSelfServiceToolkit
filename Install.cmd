15c1760424a1060799e9b3cc65b655b0c7495a0e
holbs <aranholbrook@outlook.com>
2022-02-06 22:46:09 +0000

    Scheduled task now created with PowerShell to allow it to run on battery

--- Install.cmd
+++ Install.cmd
@@ -3,6 +3,6 @@ CLS
 MD %ALLUSERSPROFILE%\Flamingo
 MD %windir%\Logs\Flamingo
 robocopy %~dp0 %ALLUSERSPROFILE%\Flamingo *.ps1 *.xaml *.ico /z /np /LOG:%windir%\Logs\Flamingo\Install.log /tee
-schtasks /create /tn "Start Flamingo" /tr "powershell -ExecutionPolicy ByPass -File %ALLUSERSPROFILE%\Flamingo\SysTray.ps1" /sc onlogon /f
+powershell Register-ScheduledTask -TaskName 'Start Flamingo' -Action (New-ScheduledTaskAction -Execute 'powershell' -Argument '-NoProfile -ExecutionPolicy bypass -File %ALLUSERSPROFILE%\Flamingo\SysTray.ps1') -Trigger (New-ScheduledTaskTrigger -AtLogon) -Settings (New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries)
 schtasks /run /tn "Start Flamingo"
 REG ADD HKLM\SOFTWARE\Flamingo /v Version /t REG_SZ /d "1.0" /f
